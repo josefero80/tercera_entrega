75c15fc86dac3fe25c37eca3dd6aada6689b74b4
Jose Ojeda <josefero80@gmail.com>
2023-01-19 22:49:32 -0500

    corregido

--- app_entrega/urls.py
+++ app_entrega/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('/resultado-busqueda-accesorios/', views.res_buscar_acc, name='buscar_acc'),
     path('/insertar-alimento', views.insertar_alimento, name='insertar_alimento'),
     path('/insertar-accesorio', views.insertar_accesorio, name='insertar_accesorio'),
-    path('/juguetes-mascotas/', views.juguetes, name='juguetes_mascotas'),
+    path('/juguetes-mascotas/', views.juguetes, name='juguete_mascotas'),
     path('/resultado-busqueda-juguetes/', views.res_buscar_ju, name='buscar_ju'),
     path('/insertar-juguete', views.insertar_ju, name='insertar_juguete'),
    
