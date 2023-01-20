67c28fe95b6dd1200cac436fe965a0d33d942f49
Jose Ojeda <josefero80@gmail.com>
2023-01-19 22:55:43 -0500

    corregido fallas

--- app_entrega/urls.py
+++ app_entrega/urls.py
@@ -3,14 +3,14 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='homepage'),
-    path('/alimento-mascotas/', views.alimentos, name='alimento_mascotas'),
-    path('/resultado-busqueda-alimento/', views.res_buscar_al, name='buscar_al'),
-    path('/accesorios-mascotas/', views.accesorios, name='accesorios_mascotas'  ),
-    path('/resultado-busqueda-accesorios/', views.res_buscar_acc, name='buscar_acc'),
-    path('/insertar-alimento', views.insertar_alimento, name='insertar_alimento'),
-    path('/insertar-accesorio', views.insertar_accesorio, name='insertar_accesorio'),
-    path('/juguetes-mascotas/', views.juguetes, name='juguete_mascotas'),
-    path('/resultado-busqueda-juguetes/', views.res_buscar_ju, name='buscar_ju'),
-    path('/insertar-juguete', views.insertar_ju, name='insertar_juguete'),
+    path('alimento-mascotas/', views.alimentos, name='alimento_mascotas'),
+    path('resultado-busqueda-alimento/', views.res_buscar_al, name='buscar_al'),
+    path('accesorios-mascotas/', views.accesorios, name='accesorios_mascotas'  ),
+    path('resultado-busqueda-accesorios/', views.res_buscar_acc, name='buscar_acc'),
+    path('insertar-alimento/', views.insertar_alimento, name='insertar_alimento'),
+    path('insertar-accesorio/', views.insertar_accesorio, name='insertar_accesorio'),
+    path('juguetes-mascotas/', views.juguetes, name='juguete_mascotas'),
+    path('resultado-busqueda-juguetes/', views.res_buscar_ju, name='buscar_ju'),
+    path('insertar-juguete/', views.insertar_ju, name='insertar_juguete'),
    
 ]
